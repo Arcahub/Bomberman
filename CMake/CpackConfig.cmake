e8ecb7c2825a0cca41a866bb64d64b62a2c36e67
Arcarox <arcarox@localhost.localdomain>
2021-05-19 10:31:19 +0200

    Initial commit

---
+++ CMake/CpackConfig.cmake
@@ -0,0 +1,42 @@
+# installer rules.
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "A 3D Bomberman")
+set(CPACK_PACKAGE_VENDOR "Indie")
+set(CPACK_PACKAGE_VERSION ${PROJECT_VER})
+set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VER_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VER_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VER_PATCH})
+set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL OFF)
+set(CPACK_NSIS_MODIFY_PATH ON)
+
+# if you have an icon set the path here
+# SET(CPACK_NSIS_MUI_ICON "${CMAKE_CURRENT_SOURCE_DIR}/my_cool_icon.ico")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "Indie\\\\bomberman")
+set(CPACK_NSIS_INSTALLED_ICON_NAME "bin\\\\bomberman.exe")
+set(CPACK_NSIS_DISPLAY_NAME "Bomberman")
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/EULA.txt")
+
+include(CPackComponent)
+
+cpack_add_install_type(Full DISPLAY_NAME "Install Everything")
+cpack_add_install_type(Upgrade DISPLAY_NAME "Upgrade Only")
+
+cpack_add_component(binaries
+    DISPLAY_NAME "Main application"
+    DESCRIPTION "This will install the main application."
+    REQUIRED
+    INSTALL_TYPES Full Upgrade
+)
+
+cpack_add_component(data_files
+    DISPLAY_NAME "Data files"
+    DESCRIPTION "Fake data files for this example."
+    INSTALL_TYPES Full
+)
+
+set(CPACK_COMPONENTS_ALL binaries data_files)
+if (CMAKE_CL_64)
+    set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64")
+else (CMAKE_CL_64)
+    set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES")
+endif (CMAKE_CL_64)
+include(CPack)
---
+++ game/src/main.cpp
@@ -0,0 +1,73 @@
+#include "ige.hpp"
+#include <iostream>
+#include <optional>
+
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+; // TODO: https://bit.ly/3hhMJ58
+
+using glm::vec3;
+using glm::vec4;
+using ige::asset::Material;
+using ige::asset::Mesh;
+using ige::asset::Texture;
+using ige::core::App;
+using ige::core::EventChannel;
+using ige::core::State;
+using ige::ecs::Schedule;
+using ige::plugin::MeshRenderer;
+using ige::plugin::PerspectiveCamera;
+using ige::plugin::RenderingPlugin;
+using ige::plugin::Transform;
+using ige::plugin::TransformPlugin;
+using ige::plugin::WindowEvent;
+using ige::plugin::WindowEventKind;
+using ige::plugin::WindowingPlugin;
+using ige::plugin::WindowSettings;
+
+class RootState : public State {
+    std::optional<EventChannel<WindowEvent>::Subscription> m_win_events;
+
+    void on_start(App& app) override
+    {
+        auto mesh = Mesh::make_cube(1.0f);
+        auto material = Material::load_default();
+        auto texture = Texture::load_file("examples/texture.png");
+
+        material->set("base_color", texture);
+
+        auto camera = app.world().create_entity();
+        camera.add_component(Transform::make_look_at(vec3(3.0f), vec3(0.0f)));
+        camera.emplace_component<PerspectiveCamera>(90.0f);
+
+        auto cube = app.world().create_entity();
+        cube.emplace_component<Transform>();
+        cube.emplace_component<MeshRenderer>(mesh, material);
+
+        auto channel = app.world().get<EventChannel<WindowEvent>>();
+        m_win_events.emplace(channel->get().subscribe());
+    }
+
+    void on_update(App& app) override
+    {
+        while (const auto& event = m_win_events->next_event()) {
+            if (event->get().kind == WindowEventKind::WindowClose) {
+                app.quit();
+            }
+        }
+    }
+};
+
+int main()
+{
+    std::cout << "Starting application..." << std::endl;
+
+    App::Builder()
+        .insert(WindowSettings { "Hello, World!", 800, 600 })
+        .add_plugin(TransformPlugin {})
+        .add_plugin(RenderingPlugin {})
+        .add_plugin(WindowingPlugin {})
+        .run<RootState>();
+
+    std::cout << "Bye bye!" << std::endl;
+}
