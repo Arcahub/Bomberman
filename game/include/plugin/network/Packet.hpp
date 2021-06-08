a6e363173fc0d96bcfc3e171b9edcb55c47f40dc
Arcarox <arcarox@localhost.localdomain>
2021-06-08 21:21:30 +0200

    Fix windows build

--- game/include/plugin/network/Packet.hpp
+++ game/include/plugin/network/Packet.hpp
@@ -1,32 +1,35 @@
 #ifndef A0B9D5BE_C26E_4C02_8171_E6D4CB4813D4
 #define A0B9D5BE_C26E_4C02_8171_E6D4CB4813D4
 
+#include <cstdint>
 #include <deque>
 #include <optional>
 
 // struct PacketFormat {
-//     u_int8_t header = 0xBC;
+//     uint8_t header = 0xBC;
 //     u_int32_t length;
-//     std::vector<u_int8_t> data;
+//     std::vector<uint8_t> data;
 // };
 
 class Packet {
 public:
     Packet();
 
-    void set_data(const std::deque<u_int8_t>& data);
-    std::optional<std::deque<u_int8_t>> get_data() const;
+    virtual ~Packet() = default;
 
-    virtual std::deque<u_int8_t> serialize() const;
-    virtual void serialize(std::deque<u_int8_t>& buff) const;
+    void set_data(const std::deque<uint8_t>& data);
+    std::optional<std::deque<uint8_t>> get_data() const;
+
+    virtual std::deque<uint8_t> serialize() const;
+    virtual void serialize(std::deque<uint8_t>& buff) const;
 
     virtual bool
-    deserialize(const std::deque<u_int8_t>& data, size_t& bytes_read);
+    deserialize(const std::deque<uint8_t>& data, size_t& bytes_read);
 
     virtual bool is_complete() const;
 
 protected:
-    std::deque<u_int8_t> m_data;
+    std::deque<uint8_t> m_data;
 
 private:
     enum class PacketState {
